a19b602918c6792a5c32211a8deb5a8cd5ac29b2
Nahu <nahucars100@outlook.com>
2025-06-19 23:37:46 -0300

    mejora ligera en el tabulado de la factura

--- LastProyecto/FormFactura.cs
+++ LastProyecto/FormFactura.cs
@@ -15,7 +15,7 @@ namespace LastProyecto
         public FormFactura(int numfac, string fecha, double cuitcliente, string razoncliente, int cantidad, string id, int preciouni, int total)
         {
             InitializeComponent();
-            txtFactura.Text = "FACTURA \t\t\t I \r\nNUMERO DE FACTURA: "+ numfac + "\t\t I \r\nFECHA: " + fecha + "\t\t I \r\nCUIT CLIENTE: " + cuitcliente + "\t\t\t I \r\nRAZON CLIENTE: " + razoncliente + "\t I \r\nCUIL EMPRESA: " /*+ cuilemp*/ + "\t\t I \r\n---------------------------------------------------------------------------------------------------------------------------------------------------------------------------\r\nCANT PROD\tID\tDESCRIPCION\t\tPRECIO UNITARIO \tSUBTOTAL \r\n" + cantidad + "\t" + id + "\t\t\t" + preciouni + "\t" +total;
+            txtFactura.Text = "FACTURA \t\t\t I \r\nNUMERO DE FACTURA: "+ numfac + "\t\t I \r\nFECHA: " + fecha + "\t\t\t I \r\nCUIT CLIENTE: " + cuitcliente + "\t\t\t I \r\nRAZON CLIENTE: " + razoncliente + "\t I \r\nCUIL EMPRESA: " /*+ cuilemp*/ + "\t\t\t I \r\n---------------------------------------------------------------------------------------------------------------------------------------------------------------------------\r\nCANT PROD\tID\tDESCRIPCION\t\tPRECIO UNITARIO \tSUBTOTAL \r\n" + cantidad + "\t\t" + id + "\t\t\t\t" + preciouni + "\t\t\t" +total;
         }
     }
 }
